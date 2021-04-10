db035c149014aa07c011469de45a1e9e8e49ca10
Odubolaoluwatimilehin <odubolaoluwatimilehin@gmail.com>
2021-04-10 19:57:52 +0100

    toggle feature

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
 
 let color = '#3aa757';
 let originalBgColor = '#ffffff';
+let scrollToggle = false;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   chrome.storage.sync.set({ originalBgColor });
+  chrome.storage.sync.set({ scrollToggle });
   console.log('Default background color set to %cgreen', `color: ${color}`);
-   console.log('Default background color set to %cgreen', `color: ${originalBgColor}`);
+  console.log( `scrollToggle: ${scrollToggle}`);
+  console.log('Default background color set to %cgreen', `color: ${originalBgColor}`);
+
 });
