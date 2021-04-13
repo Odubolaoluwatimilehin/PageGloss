cc9dc151f8e8009e04b8b4db2081b44b5df090c7
Taiwo Ogunola <taiwo.ogunola.dev@gmail.com>
2021-04-13 11:33:56 +0100

    Lint codebase

--- popup.js
+++ popup.js
@@ -1,76 +1,63 @@
-
 // Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
-let turnoffColor = document.getElementById("turnoffColor");
+let changeColor = document.getElementById('changeColor');
+let turnoffColor = document.getElementById('turnoffColor');
 
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+chrome.storage.sync.get('color', ({ color }) => {
+	changeColor.style.backgroundColor = color;
 });
 // The body of this function will be executed as a content script inside the
 // current page
 function setPageBackgroundColor() {
-  chrome.storage.sync.get("pickColor", ({ pickColor }) => {
-    document.body.style.backgroundColor = pickColor;
-  });
- 
+	chrome.storage.sync.get('pickColor', ({ pickColor }) => {
+		document.body.style.backgroundColor = pickColor;
+	});
 }
 function defaultBackgroundColor() {
-  chrome.storage.sync.get("originalBgColor", ({ originalBgColor }) => {
-    document.body.style.backgroundColor = originalBgColor;
-  });
- 
+	chrome.storage.sync.get('originalBgColor', ({ originalBgColor }) => {
+		document.body.style.backgroundColor = originalBgColor;
+	});
 }
-
-
 
 // TOGGLE EVENT LISTENER
 
 function toggleEventListener() {
- //let scrollToggle =  chrome.storage.sync.get("scrollToggle", ({ scrollToggle }) => { scrollToggle });
- //console.log(scrollToggle);
-let scrollToggle = false;
+	//let scrollToggle =  chrome.storage.sync.get("scrollToggle", ({ scrollToggle }) => { scrollToggle });
+	//console.log(scrollToggle);
+	let scrollToggle = false;
 
-    if (scrollToggle === false) {
+	if (scrollToggle === false) {
+		chrome.storage.sync.get('pickColor', ({ pickColor }) => {
+			document.body.style.backgroundColor = pickColor;
+		});
 
-      chrome.storage.sync.get("pickColor", ({ pickColor }) => {
-        document.body.style.backgroundColor = pickColor;
-      });
-    
-      scrollToggle = true;
-      console.log(scrollToggle);
+		scrollToggle = true;
+		console.log(scrollToggle);
+	} else {
+		chrome.storage.sync.get('originalBgColor', ({ originalBgColor }) => {
+			document.body.style.backgroundColor = originalBgColor;
+		});
 
-    } else {
-
-      chrome.storage.sync.get("originalBgColor", ({ originalBgColor }) => {
-        document.body.style.backgroundColor = originalBgColor;
-      });
-
-      chrome.storage.sync.set({ scrollToggle: false});
-
-    }
+		chrome.storage.sync.set({ scrollToggle: false });
+	}
 }
 
-
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-let pickColor = await document.getElementById("myColor").value; 
-chrome.storage.sync.set({ pickColor });
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+changeColor.addEventListener('click', async () => {
+	let pickColor = await document.getElementById('myColor').value;
+	chrome.storage.sync.set({ pickColor });
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: toggleEventListener,
-  });
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: toggleEventListener,
+	});
 });
 
-turnoffColor.addEventListener("click", async () => {
+turnoffColor.addEventListener('click', async () => {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: defaultBackgroundColor,
-  });
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: defaultBackgroundColor,
+	});
 });
-
